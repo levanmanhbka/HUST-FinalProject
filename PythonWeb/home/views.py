682d342e68dd1bbd5d5c0b7c6ce914c69ce5c32d
levanmanhbka <levanmanhbka@gmail.com>
2019-05-16 01:13:14 +0700

    add image gallery

--- PythonWeb/home/views.py
+++ PythonWeb/home/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, "pages/home.html")
-
-def contact(request):
-    return render(request, "pages/contact.html")
+    images = ["images/nature.jpg", "images/nature.jpg", "images/nature.jpg", "images/nature.jpg", "images/nature.jpg"]
+    return render(request, "pages/home.html", {'images':images})
