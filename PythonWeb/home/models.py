5013e2ff3911628e9bff02737f7a08c68636d68f
unknown <levanmanhbka@gmail.com>
2019-05-18 15:22:41 +0700

    upload image in admin page

--- PythonWeb/home/models.py
+++ PythonWeb/home/models.py
@@ -6,7 +6,7 @@ class Lanmark(models.Model):
     title = models.CharField(max_length=1000)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    image = models.TextField(max_length=1000)
+    image = models.ImageField(null=True)
     
     def __str__(self):
         return self.title
